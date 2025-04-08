b799cfbaac520a339f4c46f16962b2e3ac9daad9
ch-unine-056472 <ch-unine-056472@noto.epfl.ch>
2025-04-08 16:54:27 +0200

    Partie 3

---
+++ visualize.R
@@ -0,0 +1,36 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "fe59487b-fde6-4145-81c6-7a2ed6a9bafc",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "épines <- seq(99,0, by = -1)\n",
+    "coupes <- seq(0,99)\n",
+    "print (épines)\n",
+    "print (coupes)\n",
+    "#Tache 3 :\n",
+    "plot(coupes, épines, xlab=\"Nombre de coupes\", ylab = \"Nombre d'épines\", main=\"nombre d'épine en fonction du nombre de coupe\", color=\"blue\" type=\"l\", col=\"#d84a60\", cex=2, lty = 2, lwd = 3, )"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.4.1"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
